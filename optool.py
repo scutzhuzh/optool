6bc1da2088c78b025cb6b4684f86eb9400a5a8cb
scutzhuzh <354529128@qq.com>
2018-10-18 09:32:10 +0800

    Update optool.py

--- optool.py
+++ optool.py
@@ -7,6 +7,18 @@ from scipy.optimize import linprog
 import matplotlib.pyplot as plt
 
 
+# convert S to b
+
+def cb(S):
+    B = len(S)
+    b = []
+    num = 0
+    while num < B:
+        b.insert(num, 0)
+        num += 1
+    return b
+
+
 # convert kcat,Et to Vmax
 
 def kc2Vm(kc, E):
@@ -32,17 +44,17 @@ def convert(bounds):
 
 # search for the enzyme
 
-def op(c, A, b, kc, E, Enzyme):
+def op(c, A, kc, E, Enzyme):
     bounds = kc2Vm(kc, E)
     B0 = convert(bounds)
     EN = []
     Output = []
+    b = cb(A)
 
     # calculate the initial output
     res = linprog(c, A_eq=A, b_eq=b, bounds=B0)
     f0 = -1 * res.fun
     fi = -1 * res.fun
-
 
     # set times of calculation circle
     r = len(bounds) - 1
@@ -59,7 +71,7 @@ def op(c, A, b, kc, E, Enzyme):
             if f > f0:
                 EN.append(Enzyme[i])
                 print('Enzyme:', Enzyme[i])
-                Output.append(f/fi)
+                Output.append(f / fi)
                 print(' ')
                 break
             else:
@@ -94,11 +106,12 @@ def op(c, A, b, kc, E, Enzyme):
 
 # predict output
 
-def prd(Km, S, c, A, b, bounds):
+def prd(Km, c, A, E, bounds):
     # M-M equation
-    v = bounds[0] * (S / (Km[0] + S))
+    v = bounds[0] * (E[0] / (Km[0] + E[0]))
     bounds[0] = v
     B = convert(bounds)
+    b = cb(A)
     res = linprog(c, A_eq=A, b_eq=b, bounds=B)
     output = res.fun * -1
-    print('Output=',output)
+    print('Output=', output)
